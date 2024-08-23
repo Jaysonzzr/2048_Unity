4e409bd87ecc19325c8d9c99426f80952430ff31
Jaysonzzr <jaysonzr2002@gmail.com>
2024-08-23 18:25:13 -0400

    Update Colors

--- 2048/Assets/Scripts/Tile.cs
+++ 2048/Assets/Scripts/Tile.cs
@@ -67,18 +67,18 @@ public class Tile : MonoBehaviour
         // 根据Tile的数值返回不同的颜色
         switch (value)
         {
-            case 2: return new Color(0.93f, 0.89f, 0.85f, 1); // 浅黄色
-            case 4: return new Color(0.93f, 0.88f, 0.78f, 1); // 浅橙色
-            case 8: return new Color(0.95f, 0.69f, 0.47f, 1); // 橙色
-            case 16: return new Color(0.96f, 0.58f, 0.39f, 1); // 深橙色
-            case 32: return new Color(0.96f, 0.48f, 0.37f, 1); // 红橙色
-            case 64: return new Color(0.96f, 0.36f, 0.23f, 1); // 红色
-            case 128: return new Color(0.93f, 0.81f, 0.45f, 1); // 黄色
-            case 256: return new Color(0.93f, 0.80f, 0.38f, 1); // 金黄色
-            case 512: return new Color(0.93f, 0.78f, 0.31f, 1); // 金黄色
-            case 1024: return new Color(0.93f, 0.76f, 0.24f, 1); // 金黄色
-            case 2048: return new Color(0.93f, 0.75f, 0.16f, 1); // 金黄色
-            default: return Color.white; // 默认白色
+            case 2: return new Color(0.93f, 0.89f, 0.85f, 1);
+            case 4: return new Color(0.93f, 0.88f, 0.78f, 1);
+            case 8: return new Color(0.95f, 0.69f, 0.47f, 1);
+            case 16: return new Color(0.96f, 0.58f, 0.39f, 1);
+            case 32: return new Color(0.96f, 0.48f, 0.37f, 1);
+            case 64: return new Color(0.96f, 0.37f, 0.23f, 1);
+            case 128: return new Color(0.93f, 0.81f, 0.45f, 1);
+            case 256: return new Color(0.93f, 0.80f, 0.38f, 1);
+            case 512: return new Color(0.93f, 0.78f, 0.31f, 1);
+            case 1024: return new Color(0.93f, 0.76f, 0.24f, 1);
+            case 2048: return new Color(0.93f, 0.75f, 0.16f, 1);
+            default: return Color.white;
         }
     }
 }
